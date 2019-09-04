8c059e4a78a80fbfad9f3bf50da5c3c76a047369
Wyatt Goodwin <wog6@cornell.edu>
2019-09-04 22:44:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board = ["","","","","","","","","",])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{boad[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{boad[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
